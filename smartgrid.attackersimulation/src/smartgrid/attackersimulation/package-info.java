51a50125f519e6524a49aeaf0556ca64e0eebd8f
MishaStrittmatter <strittmatter@kit.edu>
2015-06-26 12:32:08 +0000

    rollback :(
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@27960 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.attackersimulation/src/smartgrid/attackersimulation/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Christian
- *
- */
-package smartgrid.attackersimulation;
--- smartgrid.helper/src/smartgrid/helper/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package Contains helper Classes for the Smart Grid Analysis
- * 
- * @author Christian
- *
- */
-package smartgrid.helper;
