69ed367e40ee453eac865c4ce6b1a725b30e0e25
sJunker <sjunker>
2015-08-10 14:58:47 +0000

    added method for adding IKritisSimulation to combobox
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@28742 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.helper/src/smartgrid/helper/ReceiveAnalysesHelper.java
+++ smartgrid.helper/src/smartgrid/helper/ReceiveAnalysesHelper.java
@@ -11,90 +11,93 @@ import org.eclipse.core.runtime.RegistryFactory;
 import smartgrid.simcontrol.baselib.Constants;
 import smartgrid.simcontrol.interfaces.IAttackerSimulation;
 import smartgrid.simcontrol.interfaces.IImpactAnalysis;
+import smartgrid.simcontrol.interfaces.IKritisSimulation;
 import smartgrid.simcontrol.interfaces.IPowerLoadSimulation;
 import smartgrid.simcontrol.interfaces.ITerminationCondition;
 import smartgrid.simcontrol.interfaces.ITimeProgressor;
 
 public class ReceiveAnalysesHelper {
 
-    private IExtensionRegistry registry = RegistryFactory.getRegistry();
+	private IExtensionRegistry registry = RegistryFactory.getRegistry();
 
-    public ReceiveAnalysesHelper() {
-    }
+	public ReceiveAnalysesHelper() {
+	}
 
-    public List<IPowerLoadSimulation> getPowerLoadElements() throws CoreException {
-        List<IPowerLoadSimulation> list = new ArrayList<IPowerLoadSimulation>();
-        IConfigurationElement[] elements = registry
-                .getConfigurationElementsFor(Constants.POWER_LOAD_SIMULATION_EXTENSION_POINT);
-        for (IConfigurationElement element : elements) {
-            Object o = element.createExecutableExtension("powerLoadSimulation");
-            if (o instanceof IPowerLoadSimulation) {
-                list.add((IPowerLoadSimulation) o);
-            }
-        }
-        return list;
-    }
+	public List<IPowerLoadSimulation> getPowerLoadElements() throws CoreException {
+		List<IPowerLoadSimulation> list = new ArrayList<IPowerLoadSimulation>();
+		IConfigurationElement[] elements = registry
+				.getConfigurationElementsFor(Constants.POWER_LOAD_SIMULATION_EXTENSION_POINT);
+		for (IConfigurationElement element : elements) {
+			Object o = element.createExecutableExtension("powerLoadSimulation");
+			if (o instanceof IPowerLoadSimulation) {
+				list.add((IPowerLoadSimulation) o);
+			}
+		}
+		return list;
+	}
 
-    public List<IAttackerSimulation> getAttackerSimulationElements() throws CoreException {
-        List<IAttackerSimulation> list = new ArrayList<IAttackerSimulation>();
-        IConfigurationElement[] elements = registry
-                .getConfigurationElementsFor(Constants.ATTACKER_SIMULATION_EXTENSION_POINT);
-        for (IConfigurationElement element : elements) {
-            Object o = element.createExecutableExtension("class");
-            if (o instanceof IAttackerSimulation) {
-                list.add((IAttackerSimulation) o);
-            }
-        }
-        return list;
-    }
+	public List<IAttackerSimulation> getAttackerSimulationElements() throws CoreException {
+		List<IAttackerSimulation> list = new ArrayList<IAttackerSimulation>();
+		IConfigurationElement[] elements = registry
+				.getConfigurationElementsFor(Constants.ATTACKER_SIMULATION_EXTENSION_POINT);
+		for (IConfigurationElement element : elements) {
+			Object o = element.createExecutableExtension("class");
+			if (o instanceof IAttackerSimulation) {
+				list.add((IAttackerSimulation) o);
+			}
+		}
+		return list;
+	}
 
-    /**
-     * Not implemented yet
-     * 
-     * @param registry
-     * @return
-     */
-    public List<String> getKritisSimulationElements() {
-        // TODO: Implement this when ready
-        return null;
-    }
+	public List<ITerminationCondition> getTerminationConditionElements() throws CoreException {
+		List<ITerminationCondition> list = new ArrayList<ITerminationCondition>();
+		IConfigurationElement[] elements = registry
+				.getConfigurationElementsFor(Constants.TERMINATION_CONDITION_EXTENSION_POINT);
+		for (IConfigurationElement element : elements) {
+			Object o = element.createExecutableExtension("class");
+			if (o instanceof ITerminationCondition) {
+				list.add((ITerminationCondition) o);
+			}
+		}
+		return list;
+	}
 
-    public List<ITerminationCondition> getTerminationConditionElements() throws CoreException {
-        List<ITerminationCondition> list = new ArrayList<ITerminationCondition>();
-        IConfigurationElement[] elements = registry
-                .getConfigurationElementsFor(Constants.TERMINATION_CONDITION_EXTENSION_POINT);
-        for (IConfigurationElement element : elements) {
-            Object o = element.createExecutableExtension("class");
-            if (o instanceof ITerminationCondition) {
-                list.add((ITerminationCondition) o);
-            }
-        }
-        return list;
-    }
+	public List<ITimeProgressor> getProgressorElements() throws CoreException {
+		List<ITimeProgressor> list = new ArrayList<ITimeProgressor>();
+		IConfigurationElement[] elements = registry
+				.getConfigurationElementsFor(Constants.TIME_PROGRESSOR_EXTENSION_POINT);
+		for (IConfigurationElement element : elements) {
+			Object o = element.createExecutableExtension("class");
+			if (o instanceof ITimeProgressor) {
+				list.add((ITimeProgressor) o);
+			}
+		}
+		return list;
+	}
 
-    public List<ITimeProgressor> getProgressorElements() throws CoreException {
-        List<ITimeProgressor> list = new ArrayList<ITimeProgressor>();
-        IConfigurationElement[] elements = registry
-                .getConfigurationElementsFor(Constants.TIME_PROGRESSOR_EXTENSION_POINT);
-        for (IConfigurationElement element : elements) {
-            Object o = element.createExecutableExtension("class");
-            if (o instanceof ITimeProgressor) {
-                list.add((ITimeProgressor) o);
-            }
-        }
-        return list;
-    }
+	public List<IImpactAnalysis> getImpactAnalysisElements() throws CoreException {
+		List<IImpactAnalysis> list = new ArrayList<IImpactAnalysis>();
+		IConfigurationElement[] elements = registry
+				.getConfigurationElementsFor(Constants.IMPACT_ANALYSIS_EXTENSION_POINT);
+		for (IConfigurationElement element : elements) {
+			Object o = element.createExecutableExtension("class");
+			if (o instanceof IImpactAnalysis) {
+				list.add((IImpactAnalysis) o);
+			}
+		}
+		return list;
+	}
 
-    public List<IImpactAnalysis> getImpactAnalysisElements() throws CoreException {
-        List<IImpactAnalysis> list = new ArrayList<IImpactAnalysis>();
-        IConfigurationElement[] elements = registry
-                .getConfigurationElementsFor(Constants.IMPACT_ANALYSIS_EXTENSION_POINT);
-        for (IConfigurationElement element : elements) {
-            Object o = element.createExecutableExtension("class");
-            if (o instanceof IImpactAnalysis) {
-                list.add((IImpactAnalysis) o);
-            }
-        }
-        return list;
-    }
+	public List<IKritisSimulation> getKritisSimulationElements() throws CoreException {
+		List<IKritisSimulation> list = new ArrayList<IKritisSimulation>();
+		IConfigurationElement[] elements = registry
+				.getConfigurationElementsFor(Constants.KRITIS_SIMULATION_EXTENSION_POINT);
+		for (IConfigurationElement element : elements) {
+			Object o = element.createExecutableExtension("class");
+			if (o instanceof IKritisSimulation) {
+				list.add((IKritisSimulation) o);
+			}
+		}
+		return list;
+	}
 }
