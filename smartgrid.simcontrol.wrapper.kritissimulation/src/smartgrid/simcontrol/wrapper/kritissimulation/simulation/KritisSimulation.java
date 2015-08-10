a0ebe3fe942750ae5de9a16c4014b46922cf6b8a
sJunker <sjunker>
2015-08-10 14:58:00 +0000

    - added wrapper plug-ins for power load simulation and kritis simulation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@28741 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ smartgrid.simcontrol.wrapper.kritissimulation/src/smartgrid/simcontrol/wrapper/kritissimulation/simulation/KritisSimulation.java
@@ -0,0 +1,17 @@
+package smartgrid.simcontrol.wrapper.kritissimulation.simulation;
+
+import java.util.List;
+
+import smartgrid.simcontrol.baselib.coupling.AbstractCostFunction;
+import smartgrid.simcontrol.baselib.coupling.IKritisSimulation;
+import smartgrid.simcontrol.baselib.coupling.PowerPerNode;
+
+public class KritisSimulation implements IKritisSimulation {
+
+	@Override
+	public List<AbstractCostFunction> doSimuation(List<PowerPerNode> power) {
+		// TODO: Implement!
+		return null;
+	}
+
+}
---
+++ smartgrid.simcontrol.wrapper.kritissimulation/src/smartgrid/simcontrol/wrapper/kritissimulation/wrapper/KritisSimulationWrapper.java
@@ -0,0 +1,42 @@
+package smartgrid.simcontrol.wrapper.kritissimulation.wrapper;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+
+import smartgrid.simcontrol.baselib.coupling.AbstractCostFunction;
+import smartgrid.simcontrol.interfaces.ErrorCodeEnum;
+import smartgrid.simcontrol.interfaces.IKritisSimulation;
+import smartgrid.simcontrol.wrapper.kritissimulation.simulation.KritisSimulation;
+import smartgridinput.ScenarioState;
+import smartgridoutput.ScenarioResult;
+import smartgridtopo.Scenario;
+
+public class KritisSimulationWrapper implements IKritisSimulation {
+
+	@Override
+	public ScenarioResult run(Scenario smartGridTopo, ScenarioState kritisInput) {
+		KritisSimulation simulation = new KritisSimulation();
+
+		// TODO: Get power per state from given parameters as parameter for
+		// simulation
+
+		List<AbstractCostFunction> list = simulation.doSimuation(null);
+
+		// TODO: Convert AbstractCostFunction list to ScenarioResult
+
+		return null;
+	}
+
+	@Override
+	public ErrorCodeEnum init(ILaunchConfiguration config) throws CoreException {
+		return ErrorCodeEnum.SUCCESS;
+	}
+
+	@Override
+	public String getName() {
+		return "KritisSimulationWrapper";
+	}
+
+}
