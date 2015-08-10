c6ca45777bf6b66d24b4d6abac7919d890d657e8
sJunker <sjunker>
2015-08-10 14:45:36 +0000

    - added new extension point for kritis simulation and added kritis simulation mock - refactored SimulationTab so that kritis simulations are visible
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@28736 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/KritisSimulationMock.java
@@ -0,0 +1,29 @@
+package smartgrid.simcontrol.mocks;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+
+import smartgrid.simcontrol.interfaces.ErrorCodeEnum;
+import smartgrid.simcontrol.interfaces.IKritisSimulation;
+import smartgridinput.ScenarioState;
+import smartgridoutput.ScenarioResult;
+import smartgridtopo.Scenario;
+
+public class KritisSimulationMock implements IKritisSimulation {
+
+	@Override
+	public ScenarioResult run(Scenario smartGridTopo, ScenarioState kritisInput) {
+		return null;
+	}
+
+	@Override
+	public ErrorCodeEnum init(ILaunchConfiguration config) throws CoreException {
+		return ErrorCodeEnum.SUCCESS;
+	}
+
+	@Override
+	public String getName() {
+		return "Kritis Simulation Mock";
+	}
+
+}
