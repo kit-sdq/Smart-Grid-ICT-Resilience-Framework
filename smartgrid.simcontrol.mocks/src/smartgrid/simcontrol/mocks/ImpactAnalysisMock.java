236011a3f623395582af12113ebd22f68f1f6243
sJunker <sjunker>
2015-08-19 06:09:40 +0000

    - impact analysis mock now returns a valid output model instead of null. Every state is now Online and there is only one cluster which contains all states
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@28831 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/ImpactAnalysisMock.java
+++ smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/ImpactAnalysisMock.java
@@ -9,40 +9,73 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import smartgrid.simcontrol.interfaces.ErrorCodeEnum;
 import smartgrid.simcontrol.interfaces.IImpactAnalysis;
 import smartgridinput.ScenarioState;
+import smartgridoutput.Cluster;
+import smartgridoutput.Online;
 import smartgridoutput.ScenarioResult;
+import smartgridoutput.SmartgridoutputFactory;
+import smartgridoutput.impl.SmartgridoutputPackageImpl;
+import smartgridtopo.ControlCenter;
 import smartgridtopo.Scenario;
+import smartgridtopo.SmartMeter;
 
 /**
  * This Class mocks the Imapct Analysis
  * 
- * @author Christian
+ * It mocks the impact analysis in that way that regardless of the input states
+ * every output state is marked as 'Online' and all states are packed in one
+ * single cluster
+ * 
+ * @author Christian, Michael
  *
  */
 public class ImpactAnalysisMock implements IImpactAnalysis {
 
 	/**
-	 * {@inheritDoc}<p>
+	 * {@inheritDoc}
+	 * <p>
 	 * 
 	 * 
 	 * NOT yet implemented!
 	 * 
 	 */
 	@Override
-	public ScenarioResult run(Scenario smartGridTopo,
-			ScenarioState impactAnalysisInput) {
-		
+	public ScenarioResult run(Scenario smartGridTopo, ScenarioState impactAnalysisInput) {
+		SmartgridoutputPackageImpl.init();
+		SmartgridoutputFactory factory = SmartgridoutputFactory.eINSTANCE;
+		ScenarioResult result = factory.createScenarioResult();
 
-		//TODO Implement
-		
-			
-		
-		
-		
-		return null;
+		result.setScenario(smartGridTopo);
+
+		int smartmetercount = 0;
+		int controlcentercount = 0;
+
+		Cluster cl = factory.createCluster();
+		for (smartgridinput.EntityState entity : impactAnalysisInput.getEntityStates()) {
+			Online on = factory.createOnline();
+			on.setOwner(entity.getOwner());
+			on.setBelongsToCluster(cl);
+
+			result.getEntityStates().add(on);
+
+			// Count smartMeters and controlCenters
+			if (entity.getOwner() instanceof SmartMeter) {
+				smartmetercount++;
+			} else if (entity.getOwner() instanceof ControlCenter) {
+				controlcentercount++;
+			}
+		}
+
+		cl.setSmartMeterCount(smartmetercount);
+		cl.setControlCenterCount(controlcentercount);
+
+		result.getClusters().add(cl);
+
+		return result;
 	}
 
 	/**
-	 * {@inheritDoc}<p>
+	 * {@inheritDoc}
+	 * <p>
 	 * 
 	 * 
 	 * NOT yet implemented!
@@ -51,15 +84,14 @@ public class ImpactAnalysisMock implements IImpactAnalysis {
 	@Override
 	public ErrorCodeEnum init(ILaunchConfiguration config) throws CoreException {
 
+		// TODO Implement
 
-		//TODO Implement
-		
 		return ErrorCodeEnum.SUCCESS;
 	}
 
-    @Override
-    public String getName() {
-        return "Impact Analyses Mock";
-    }
+	@Override
+	public String getName() {
+		return "Impact Analyses Mock";
+	}
 
 }
