16b202dd687d48ba78842877e3ec6887c9c12cf8
sJunker <sjunker>
2015-11-04 14:26:10 +0000

    - bug fixes due to mm changes - TimeProgressorMock --> No Operation - TerminationConditionMock --> Iteration Count
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@29748 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.model.helper/src/smartgrid/model/helper/output/LoadOutputModelConformityHelper.java
+++ smartgrid.model.helper/src/smartgrid/model/helper/output/LoadOutputModelConformityHelper.java
@@ -6,66 +6,67 @@ import java.util.List;
 import smartgridoutput.EntityState;
 import smartgridoutput.ScenarioResult;
 import smartgridtopo.NetworkEntity;
-import smartgridtopo.Scenario;
+import smartgridtopo.SmartGridTopology;
 
 public final class LoadOutputModelConformityHelper {
 
-    private LoadOutputModelConformityHelper() {
-    }
+	private LoadOutputModelConformityHelper() {
+	}
 
-    public static boolean checkOutputModelConformitySimple(ScenarioResult result, Scenario current) {
-        if (result.getScenario() == null) {
-            return true;
-        }
-       
-        return result.getScenario().getId() == current.getId();
-    }
+	public static boolean checkOutputModelConformitySimple(ScenarioResult result, SmartGridTopology current) {
+		if (result.getScenario() == null) {
+			return true;
+		}
 
-    public static boolean checkOutputModelConformity(ScenarioResult output, Scenario current) {
-        // Check differences that would be obvious and ignore if output.getScenario() is null
-        boolean result = checkOutputModelConformitySimple(output, current);
-         
-        if (result) {
-            return compareAndCountEntityStates(output.getEntityStates(), current.getContainsNE());
-        }
+		return result.getScenario().getId() == current.getId();
+	}
 
-        return result;
-    }
+	public static boolean checkOutputModelConformity(ScenarioResult output, SmartGridTopology current) {
+		// Check differences that would be obvious and ignore if
+		// output.getScenario() is null
+		boolean result = checkOutputModelConformitySimple(output, current);
 
-    private static boolean compareAndCountEntityStates(List<EntityState> states, List<NetworkEntity> entities) {
-        boolean result = true;
+		if (result) {
+			return compareAndCountEntityStates(output.getStates(), current.getContainsNE());
+		}
 
-        List<EntityState> noZombies = getListWithoutZombies(states);
+		return result;
+	}
 
-        if (noZombies.size() == entities.size()) {
-            for (NetworkEntity entity : entities) {
-                boolean found = false;
-                for (EntityState state : noZombies) {
-                    if (state.getOwner().getId() == entity.getId()) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    result = false;
-                }
-            }
-        } else {
-            result = false;
-        }
+	private static boolean compareAndCountEntityStates(List<EntityState> states, List<NetworkEntity> entities) {
+		boolean result = true;
 
-        return result;
-    }
+		List<EntityState> noZombies = getListWithoutZombies(states);
 
-    private static List<EntityState> getListWithoutZombies(List<EntityState> states) {
-        List<EntityState> noZombies = new ArrayList<EntityState>();
+		if (noZombies.size() == entities.size()) {
+			for (NetworkEntity entity : entities) {
+				boolean found = false;
+				for (EntityState state : noZombies) {
+					if (state.getOwner().getId() == entity.getId()) {
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					result = false;
+				}
+			}
+		} else {
+			result = false;
+		}
 
-        for (EntityState state : states) {
-            if (state.getOwner() != null) {
-                noZombies.add(state);
-            }
-        }
+		return result;
+	}
 
-        return noZombies;
-    }
+	private static List<EntityState> getListWithoutZombies(List<EntityState> states) {
+		List<EntityState> noZombies = new ArrayList<EntityState>();
+
+		for (EntityState state : states) {
+			if (state.getOwner() != null) {
+				noZombies.add(state);
+			}
+		}
+
+		return noZombies;
+	}
 }
