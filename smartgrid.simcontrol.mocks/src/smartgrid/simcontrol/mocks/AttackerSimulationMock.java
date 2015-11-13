7132137bfb772a2ee725b5bb3fc55d016f0cea5a
sJunker <sjunker>
2015-11-13 11:05:56 +0000

    - added boolean method to attacker simulation that indicates if attributes can be used or not - impact analysis option is now saved when leaving the launch configuration dialog
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@29863 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/AttackerSimulationMock.java
+++ smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/AttackerSimulationMock.java
@@ -41,4 +41,9 @@ public class AttackerSimulationMock implements IAttackerSimulation {
 		return "No Attack Simulation";
 	}
 
+	@Override
+	public boolean enableFurtherAttributes() {
+		return false;
+	}
+
 }
