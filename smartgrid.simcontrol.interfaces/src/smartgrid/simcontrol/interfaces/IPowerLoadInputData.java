45fb0f308da5ddfbeba15382073f21cd75a81864
MishaStrittmatter <strittmatter@kit.edu>
2016-05-30 13:01:30 +0000

    delete the interfaces project
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@32137 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.interfaces/src/smartgrid/simcontrol/interfaces/IPowerLoadInputData.java
+++
@@ -1,5 +0,0 @@
-package smartgrid.simcontrol.interfaces;
-
-public interface IPowerLoadInputData {
-    //Kein Plan, was hier mal später reinkommt. Hängt von meinem Forschungspartnern ab
-}
--- smartgrid.simcontrol.interfaces/src/smartgrid/simcontrol/interfaces/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package provides the Interfaces  for @see smargrid.simcontrol 
- * that have to be implemented to use with the Simcontrol Analysis
- * 
- * 
- * @author Christian
- *
- */
-package smartgrid.simcontrol.interfaces;
