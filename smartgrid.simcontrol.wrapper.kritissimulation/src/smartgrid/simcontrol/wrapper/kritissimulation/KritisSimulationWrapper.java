ebaa7df20c34e926e57a6715630aae9f742c6d89
MishaStrittmatter <strittmatter@kit.edu>
2016-06-27 13:07:20 +0000

    implemented KRITIS Wrapper
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@32332 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.wrapper.kritissimulation/src/smartgrid/simcontrol/wrapper/kritissimulation/KritisSimulationWrapper.java
+++ smartgrid.simcontrol.wrapper.kritissimulation/src/smartgrid/simcontrol/wrapper/kritissimulation/KritisSimulationWrapper.java
@@ -1,39 +1,96 @@
 package smartgrid.simcontrol.wrapper.kritissimulation;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
+import simcity.libagent.BaseAgent;
+import simcity.power.Electrified;
+import simcity.xlauncher.SimConnector;
 import smartgrid.simcontrol.baselib.ErrorCodeEnum;
-import smartgrid.simcontrol.baselib.coupling.IKritisSimulation;
 import smartgrid.simcontrol.baselib.coupling.IKritisSimulationWrapper;
 import smartgrid.simcontrol.baselib.coupling.PowerSpec;
 
 public class KritisSimulationWrapper implements IKritisSimulationWrapper {
 
+    private static final Logger LOG = Logger.getLogger(KritisSimulationWrapper.class);
+
+    private SimConnector kritisSimConnector;
+
+    @SuppressWarnings("unchecked")
     @Override
     public Map<String, PowerSpec> run(Map<String, Double> power) {
 
-        // TODO implement wrapper!
-        final IKritisSimulation dummy = new IKritisSimulation() {
-            @Override
-            public Map<String, PowerSpec> run(Map<String, Double> power) {
-                throw new RuntimeException("KRITIS wrapper not yet implemented!");
-            }
-        };
+        // get all agents with power supply
+        Iterable<Electrified> agents = kritisSimConnector.getAgents(Electrified.class);
+        
+        // set power supply
+        for (Electrified ag : agents) {
+            String agentId = ((BaseAgent) ag).getAgentId();
+            double powerSupply = power.get(agentId);
+            ag.setPowerInputWatt((int)powerSupply); //TODO: remove cast
+            
+            LOG.debug("Set power input " + powerSupply + " for agent " + agentId);
+        }
+        
+        LOG.debug("Performing KRITIS simulation step");
+        kritisSimConnector.step();
 
-        return dummy.run(power);
+        // get all agents with power supply
+        agents = kritisSimConnector.getAgents(Electrified.class);
+
+        // get power demand
+        Map<String, PowerSpec> powerDemand = new HashMap<String, PowerSpec>();
+        for (Electrified ag : agents) {
+            //TODO: use proper methods, remove cast
+            double powerRequired = (double) ag.getPowerRequired();
+            PowerSpec powerSpec = new PowerSpec(powerRequired, powerRequired);
+
+            String agentId = ((BaseAgent) ag).getAgentId();
+            
+            powerDemand.put(agentId, powerSpec);
+            LOG.debug("Got demand " + powerRequired + " from agent " + agentId);
+        }
+
+        return powerDemand;
     }
 
     @Override
     public Map<String, PowerSpec> getDefaultDemand() {
-        // TODO implement wrapper!
-        return null;
+
+        // get all agents with power supply
+        @SuppressWarnings("unchecked")
+        Iterable<Electrified> agents = kritisSimConnector.getAgents(Electrified.class);
+
+        //get default demand
+        Map<String, PowerSpec> powerDemand = new HashMap<String, PowerSpec>();
+        for (Electrified ag : agents) {
+            // TODO: use proper methods, remove cast
+            double powerRequired = (double) ag.getPowerRequired();
+            PowerSpec powerSpec = new PowerSpec(powerRequired, powerRequired);
+
+            String agentId = ((BaseAgent) ag).getAgentId();
+            
+            powerDemand.put(agentId, powerSpec);
+            LOG.debug("Got default demand " + powerRequired + " from agent " + agentId);
+        }
+        return powerDemand;
     }
 
     @Override
     public ErrorCodeEnum init(final ILaunchConfiguration config) throws CoreException {
+
+        // initialize KRITIS-model
+        kritisSimConnector = new SimConnector(
+                "/Shared_workspace/gekoppelte_simulation/IKET/TestSimulation/mysim_v1/SimCity.rs");
+
+        kritisSimConnector.initSimulation(
+                "/Shared_workspace/gekoppelte_simulation/IKET/TestSimulation/mysim_v1/agents/houseHoldAgentArray.json",
+                "/Shared_workspace/gekoppelte_simulation/IKET/TestSimulation/mysim_v1/agents/pharmacyAgentArray.json");
+
         return ErrorCodeEnum.SUCCESS;
     }
 
