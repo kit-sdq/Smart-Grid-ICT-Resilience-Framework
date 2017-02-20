899f0fc0241ce501315f54b5ab44332a1cc781fb
MishaStrittmatter <strittmatter@kit.edu>
2017-02-20 14:35:59 +0000

    minor: format
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34085 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IKritisSimulation.java
+++ smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IKritisSimulation.java
@@ -3,6 +3,6 @@ package smartgrid.simcontrol.coupling;
 import java.util.Map;
 
 public interface IKritisSimulation {
-    Map<String,PowerSpec> run(Map<String,Double> power);
+    Map<String, PowerSpec> run(Map<String, Double> power);
     Map<String, PowerSpec> getDefaultDemand();
 }
