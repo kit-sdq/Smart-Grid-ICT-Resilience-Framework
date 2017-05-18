ce08c6031017a55b8bd3a37833b2b30fd859c693
smartgrid <smartgrid>
2017-05-18 13:27:54 +0000

    PowerSpec updated
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34244 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/PowerSpec.java
+++ smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/PowerSpec.java
@@ -1,25 +1,72 @@
 package smartgrid.simcontrol.coupling;
 
 public class PowerSpec {
-    private double baseDemand; //optimal demand
-    private double actualDemand; //reduced demand during power failure
+	private String ciType;     //CI, -component ID
+	private String ciName;     //CI, -component name
+	private String ciSmartID;  //Smart meter agent-ID
+    private double optDemand; //optimal demand
+    private double minReqDemand; //Minimum required power
     private double criticality; //1 = human casualties (has to be supplied)
 
-    public double getDefaultDemand() {
-        return baseDemand;
-    }
 
-    public double getActualDemand() {
-        return actualDemand;
-    }
-    
-    public double getCriticality() {
-        return criticality;
-    }
-
-    public PowerSpec(double baseDemand, double actualDemand, double criticality) {
-        this.baseDemand = baseDemand;
-        this.actualDemand = actualDemand;
+    public PowerSpec(String ciType, String ciName, String ciSmartID, double optDemand, double minReqDemand, double criticality) {
+        this.ciType = ciType;
+        this.ciName = ciName;
+        this.ciSmartID = ciSmartID;    	
+    	this.optDemand = optDemand;
+        this.minReqDemand = minReqDemand;
         this.criticality = criticality;
     }
+
+
+	/**
+	 * @return the ciType
+	 */
+	public String getCiType() {
+		return ciType;
+	}
+
+
+	/**
+	 * @return the ciName
+	 */
+	public String getCiName() {
+		return ciName;
+	}
+
+
+	/**
+	 * @return the ciSmartID
+	 */
+	public String getCiSmartID() {
+		return ciSmartID;
+	}
+
+
+	/**
+	 * @return the baseDemand
+	 */
+	public double getBaseDemand() {
+		return optDemand;
+	}
+
+
+	/**
+	 * @return the minReqDemand
+	 */
+	public double getMinReqDemand() {
+		return minReqDemand;
+	}
+
+
+	/**
+	 * @return the criticality
+	 */
+	public double getCriticality() {
+		return criticality;
+	}
+	
 }
+
+
+
