c36d974454d8c947733119c400553459d6729bc1
MishaStrittmatter <strittmatter@kit.edu>
2017-06-23 13:33:48 +0000

    delegate shutdown
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34386 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol/src/smartgrid/simcontrol/Activator.java
+++ smartgrid.simcontrol/src/smartgrid/simcontrol/Activator.java
@@ -40,6 +40,7 @@ public class Activator extends AbstractUIPlugin {
     @Override
     public void stop(final BundleContext context) throws Exception {
         plugin = null;
+        Startup.shutDown();
         super.stop(context);
     }
 
