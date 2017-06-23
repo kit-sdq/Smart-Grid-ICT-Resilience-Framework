67b1d8e79ebf66c7e2ea9183b8a70a986eda9619
MishaStrittmatter <strittmatter@kit.edu>
2017-06-23 08:22:10 +0000

    update javadoc
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34376 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IPowerLoadSimulation.java
+++ smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IPowerLoadSimulation.java
@@ -9,7 +9,7 @@ public interface IPowerLoadSimulation {
 
     /**
      * @param kritisDemands
-     *            Map von Node ID (String) auf Liste von PowerSpecs
+     *            Map von Node ID (String) auf Map von CI ID (String) auf PowerSpecs
      * @param smartMeterStates
      *            Map von Node ID auf Map von CI ID auf SmartMeterState
      * @return Map von Node ID (String) auf Map von CI ID (String) auf Leistung (MW)
