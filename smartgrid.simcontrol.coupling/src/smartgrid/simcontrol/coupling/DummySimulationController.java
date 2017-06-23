87db1bb08c898969a82c8958fe9c2365a9a7fc72
smartgrid <smartgrid>
2017-06-23 15:08:26 +0000

    Added dummy RMI server for testing purposes.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34395 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/DummySimulationController.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package smartgrid.simcontrol.coupling;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author tim
+ *
+ */
+public class DummySimulationController implements ISimulationController {
+
+  /* (non-Javadoc)
+   * @see smartgrid.simcontrol.coupling.ISimulationController#init(java.lang.String, java.lang.String, java.lang.String)
+   */
+  @Override public void init(String outputPath, String topoPath, String inputStatePath) throws RemoteException, SimcontrolException {
+    System.out.println("RMI: init called");
+  }
+
+  /* (non-Javadoc)
+   * @see smartgrid.simcontrol.coupling.ISimulationController#run(java.util.Map)
+   */
+  @Override public Map<String, Map<String, Double>> run(Map<String, Map<String, PowerSpec>> kritisPowerDemand) throws RemoteException {
+    System.out.println("RMI: run called");
+    return new HashMap<String, Map<String, Double>>();
+  }
+
+  /* (non-Javadoc)
+   * @see smartgrid.simcontrol.coupling.ISimulationController#shutDown()
+   */
+  @Override public void shutDown() throws RemoteException {
+    System.out.println("RMI: shutdown called");
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    try {
+      // likely to work without security manager
+//      if (System.getSecurityManager() == null) {
+//        System.setSecurityManager(new SecurityManager());
+//      }
+      DummySimulationController endpoint = new DummySimulationController();
+      ISimulationController endpoint_stub = (ISimulationController )UnicastRemoteObject.exportObject(endpoint, 0);
+      Registry registry = LocateRegistry.createRegistry(1099);
+      registry.bind("ISimulationController", endpoint_stub);
+      System.out.println("RMI server initialised");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
