ac3f677980d6cd69a55a94ccf62f842009519939
MishaStrittmatter <strittmatter@kit.edu>
2017-06-23 13:43:03 +0000

    logging init only done once, synchronized init
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34391 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.log4starter/src/smartgrid/log4j/LoggingInitializer.java
+++ smartgrid.log4starter/src/smartgrid/log4j/LoggingInitializer.java
@@ -8,14 +8,16 @@ import org.apache.log4j.PropertyConfigurator;
 public final class LoggingInitializer {
 
     private static final Logger LOG = Logger.getLogger(LoggingInitializer.class);
+    private static boolean isInitialized = false;
 
     private LoggingInitializer() {
-
     }
 
-    public static void initialize() {
-        final URL url = LoggingInitializer.class.getResource("/log4j/log4j.properties");
-        PropertyConfigurator.configure(url);
-        LOG.debug("Log4j Logger initialized");
+    public static synchronized void initialize() {
+        if (!isInitialized) {
+            final URL url = LoggingInitializer.class.getResource("/log4j/log4j.properties");
+            PropertyConfigurator.configure(url);
+            LOG.debug("Log4j Logger initialized");
+        }
     }
 }
