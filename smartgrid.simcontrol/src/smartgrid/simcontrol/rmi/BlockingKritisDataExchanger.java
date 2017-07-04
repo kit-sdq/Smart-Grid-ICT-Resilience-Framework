ea710fd9339e20ad9a5415e198c996b25e60e314
MishaStrittmatter <strittmatter@kit.edu>
2017-07-04 16:15:35 +0000

    exchanger done
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34422 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol/src/smartgrid/simcontrol/rmi/BlockingKritisDataExchanger.java
+++ smartgrid.simcontrol/src/smartgrid/simcontrol/rmi/BlockingKritisDataExchanger.java
@@ -2,17 +2,44 @@ package smartgrid.simcontrol.rmi;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import smartgrid.simcontrol.coupling.PowerSpec;
 
 public class BlockingKritisDataExchanger {
+
+    private static final Logger LOG = Logger.getLogger(BlockingKritisDataExchanger.class);
+
     private static Map<String, Map<String, PowerSpec>> bufferedDemand;
     private static Map<String, Map<String, Double>> bufferedPower;
 
     public static synchronized Map<String, Map<String, PowerSpec>> passDataToKritisSim(Map<String, Map<String, Double>> power) {
-        return null;
+        assert bufferedPower == null;
+        bufferedPower = power;
+        while (bufferedDemand == null) {
+            try {
+                BlockingKritisDataExchanger.class.wait();
+            } catch (InterruptedException e) {
+                LOG.warn("Interrupted while waiting for data. This is unexpected.", e);
+            }
+        }
+        Map<String, Map<String, PowerSpec>> tempDemand = bufferedDemand;
+        bufferedDemand = null;
+        return tempDemand;
     }
 
     public static synchronized Map<String, Map<String, Double>> getDataFromCoupling(Map<String, Map<String, PowerSpec>> demand) {
-        return null;
+        assert bufferedDemand == null;
+        bufferedDemand = demand;
+        while (bufferedPower == null) {
+            try {
+                BlockingKritisDataExchanger.class.wait();
+            } catch (InterruptedException e) {
+                LOG.warn("Interrupted while waiting for data. This is unexpected.", e);
+            }
+        }
+        Map<String, Map<String, Double>> tempPower = bufferedPower;
+        bufferedPower = null;
+        return tempPower;
     }
 }
