e2784012ed2e35afbc79f50ee7dd2707cfd96a5f
MishaStrittmatter <strittmatter@kit.edu>
2017-07-10 14:52:46 +0000

    fixed KRIT mock, fixed warnings
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34450 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/KritisSimulationMock.java
+++ smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/KritisSimulationMock.java
@@ -3,7 +3,6 @@ package smartgrid.simcontrol.mocks;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
 import smartgrid.simcontrol.baselib.ErrorCodeEnum;
@@ -13,17 +12,17 @@ import smartgrid.simcontrol.coupling.PowerSpec;
 public class KritisSimulationMock implements IKritisSimulationWrapper {
 
     @Override
-    public Map<String, PowerSpec> run(Map<String, Double> power) {
-        return new HashMap<String, PowerSpec>();
+    public Map<String, Map<String, PowerSpec>> run(Map<String, Map<String, Double>> power) {
+        return new HashMap<String, Map<String, PowerSpec>>(); // TODO at least transfer structure from power to outpout
     }
 
     @Override
-    public Map<String, PowerSpec> getDefaultDemand() {
-        return new HashMap<String, PowerSpec>();
+    public Map<String, Map<String, PowerSpec>> getDefaultDemand() {
+        return new HashMap<String, Map<String, PowerSpec>>(); // TODO return more meaningful values?
     }
 
     @Override
-    public ErrorCodeEnum init(final ILaunchConfiguration config) throws CoreException {
+    public ErrorCodeEnum init(final ILaunchConfiguration config) {
         return ErrorCodeEnum.SUCCESS;
     }
 
--- smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/PowerLoadSimulationMock.java
+++ smartgrid.simcontrol.mocks/src/smartgrid/simcontrol/mocks/PowerLoadSimulationMock.java
@@ -6,7 +6,6 @@ package smartgrid.simcontrol.mocks;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
 import smartgrid.simcontrol.baselib.ErrorCodeEnum;
@@ -27,11 +26,11 @@ public class PowerLoadSimulationMock implements IPowerLoadSimulationWrapper {
      */
     @Override
     public Map<String, Map<String, Double>> run(Map<String, Map<String, PowerSpec>> kritisDemands, Map<String, Map<String, ISmartMeterState>> smartMeterStates) {
-        return new HashMap<String, Map<String,Double>>();
+        return new HashMap<String, Map<String, Double>>();
     }
 
     @Override
-    public ErrorCodeEnum init(final ILaunchConfiguration config) throws CoreException {
+    public ErrorCodeEnum init(final ILaunchConfiguration config) {
         return ErrorCodeEnum.SUCCESS;
     }
 
