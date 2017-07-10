1781b1aad47fff1ec5f08bffd14118ec5c7a47f0
MishaStrittmatter <strittmatter@kit.edu>
2017-07-10 14:47:55 +0000

    update KRITIS interface to nested maps
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34446 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IKritisSimulation.java
+++ smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IKritisSimulation.java
@@ -3,6 +3,7 @@ package smartgrid.simcontrol.coupling;
 import java.util.Map;
 
 public interface IKritisSimulation {
-    Map<String, PowerSpec> run(Map<String, Double> power);
-    Map<String, PowerSpec> getDefaultDemand();
+    Map<String, Map<String, PowerSpec>> run(Map<String, Map<String, Double>> power);
+
+    Map<String, Map<String, PowerSpec>> getDefaultDemand();
 }
