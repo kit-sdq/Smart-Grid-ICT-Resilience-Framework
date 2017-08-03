7f1a0c731679ce49003ad534289ece26bc438615
MishaStrittmatter <strittmatter@kit.edu>
2017-08-03 12:42:59 +0000

    Add toolbar button
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34535 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ smartgrid.simcontrol.ui/src/smartgrid/simcontrol/ui/toolbar/InterruptButtonHandler.java
@@ -0,0 +1,14 @@
+package smartgrid.simcontrol.ui.toolbar;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class InterruptButtonHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
