7b9860f18de6547d1cdf2ee9e1f36cc6915e420d
MishaStrittmatter <strittmatter@kit.edu>
2017-08-03 14:44:23 +0000

    implemented freeing of waiting threads
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34536 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol/src/smartgrid/simcontrol/rmi/BlockingKritisDataExchanger.java
+++ smartgrid.simcontrol/src/smartgrid/simcontrol/rmi/BlockingKritisDataExchanger.java
@@ -16,13 +16,17 @@ public class BlockingKritisDataExchanger {
     private static final Logger LOG = Logger.getLogger(BlockingKritisDataExchanger.class);
 
     private static Map<String, Map<String, PowerSpec>> bufferedDemand;
+    private static Thread kritisThread;
+
     private static Map<String, Map<String, Double>> bufferedPower;
+    private static Thread couplingThread;
 
     public static synchronized Map<String, Map<String, PowerSpec>> passDataToKritisSim(Map<String, Map<String, Double>> power) throws InterruptedException {
         assert bufferedPower == null;
 
         // provide own data
         bufferedPower = power;
+        couplingThread = Thread.currentThread();
         BlockingKritisDataExchanger.class.notifyAll();
 
         // wait for data
@@ -34,6 +38,7 @@ public class BlockingKritisDataExchanger {
         // consume data
         Map<String, Map<String, PowerSpec>> tempDemand = bufferedDemand;
         bufferedDemand = null;
+        couplingThread = null;
         LOG.info("SimControl has finished its exchange.");
         return tempDemand;
     }
@@ -43,6 +48,7 @@ public class BlockingKritisDataExchanger {
 
         // provide own data
         bufferedDemand = demand;
+        kritisThread = Thread.currentThread();
         BlockingKritisDataExchanger.class.notifyAll();
 
         // wait for data
@@ -54,7 +60,21 @@ public class BlockingKritisDataExchanger {
         // consume data
         Map<String, Map<String, Double>> tempPower = bufferedPower;
         bufferedPower = null;
+        kritisThread = null;
         LOG.info("The Kritis simulation has finished its exchange.");
         return tempPower;
+    }
+
+    public static synchronized boolean freeAll() {
+        boolean threadFreed = couplingThread != null || kritisThread != null;
+        if (couplingThread != null) {
+            couplingThread.interrupt();
+            couplingThread = null;
+        }
+        if (kritisThread != null) {
+            kritisThread.interrupt();
+            kritisThread = null;
+        }
+        return threadFreed;
     }
 }
