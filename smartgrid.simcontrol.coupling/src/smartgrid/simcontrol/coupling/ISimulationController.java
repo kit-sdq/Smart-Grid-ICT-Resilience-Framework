75e31977ea72f38adb7eb909372b211b2e9d01e2
smartgrid <smartgrid>
2017-08-04 14:19:40 +0000

    Added alpha javadoc to interface.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34542 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/ISimulationController.java
+++ smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/ISimulationController.java
@@ -20,8 +20,22 @@ public interface ISimulationController extends Remote {
     //input data for initializing ICT/power grid -topology
     void initTopo(Map<String, Map<String, SmartMeterGeoData>> _smartMeterGeoData) throws RemoteException, SimcontrolException;
 
-    //receiving calculated suppliable power
+    /**
+     * Get a list of power supplied to ... 
+     * Remotely aborting leaves an undefined state and requires the connection to be reinitialised before continuing.
+     * @param kritisPowerDemand
+     * @return 
+     * @throws RemoteException if 
+     * @throws SimcontrolException
+     * @throws InterruptedException if aborted remotely
+     */
     Map<String, Map<String, Double>> runAndGetPowerSupplied(Map<String, Map<String, PowerSpec>> kritisPowerDemand) throws RemoteException, SimcontrolException, InterruptedException;
 
+    /**
+     * Signals the remote application to perform a controlled shutdown.
+     * @throws RemoteException
+     * @throws SimcontrolException
+     */
     void shutDown() throws RemoteException, SimcontrolException;
+ 
 }
