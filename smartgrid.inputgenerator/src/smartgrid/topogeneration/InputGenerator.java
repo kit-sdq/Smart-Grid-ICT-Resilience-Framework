9c3606f5cbe110f626a2523ce914eb32aadbfc1c
MishaStrittmatter <strittmatter@kit.edu>
2017-10-11 11:58:39 +0000

    working on "trivial" topo generation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34640 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.inputgenerator/src/smartgrid/topogeneration/InputGenerator.java
+++ smartgrid.inputgenerator/src/smartgrid/topogeneration/InputGenerator.java
@@ -1,52 +1,110 @@
-/**
- *
- */
 package smartgrid.topogeneration;
 
-import smartgrid.simcontrol.baselib.GenerationStyle;
-import smartgridinput.ScenarioState;
-import smartgridinput.SmartgridinputFactory;
-import smartgridinput.impl.SmartgridinputPackageImpl;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.log4j.Logger;
+
+import smartgrid.simcontrol.coupling.SmartMeterGeoData;
+import smartgridtopo.ControlCenter;
+import smartgridtopo.LogicalCommunication;
+import smartgridtopo.NamedIdentifier;
+import smartgridtopo.NetworkNode;
+import smartgridtopo.PhysicalConnection;
+import smartgridtopo.PowerGridNode;
 import smartgridtopo.SmartGridTopology;
+import smartgridtopo.SmartMeter;
 import smartgridtopo.SmartgridtopoFactory;
 import smartgridtopo.impl.SmartgridtopoPackageImpl;
 
-/**
- * @author Christian
- *
- */
-public class InputGenerator implements IInputGenerator {
+public class InputGenerator {
 
-    /**
-     *
-     */
-    public InputGenerator() {
-    }
+    private static final Logger LOG = Logger.getLogger(InputGenerator.class);
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see smartgrid.simcontrol.interfaces.IInputGenerator#init()
-     */
-    @Override
-    public void init() {
-    }
+    public void generateTopo(Map<String, Map<String, SmartMeterGeoData>> smartMeterGeoData) {
 
-    @Override
-    public InputModelDTO generate(final GenerationStyle desiredStyle, final int SmartMeterCount, final int ControlCenterCount) {
-
-        // Init Scenario State
-        SmartgridinputPackageImpl.init();
-        final SmartgridinputFactory scenarioStateFactory = SmartgridinputFactory.eINSTANCE;
-        final ScenarioState myScenarioStates = scenarioStateFactory.createScenarioState();
-
-        // Init Scenario Topo
+        // create root container
         SmartgridtopoPackageImpl.init();
-        final SmartgridtopoFactory scenarioTopoFactore = SmartgridtopoFactory.eINSTANCE;
-        final SmartGridTopology myScenarioTopo = scenarioTopoFactore.createSmartGridTopology();
+        final SmartgridtopoFactory topoFactory = SmartgridtopoFactory.eINSTANCE;
+        final SmartGridTopology topo = topoFactory.createSmartGridTopology();
 
-        final InputModelDTO myDTO = new InputModelDTO(myScenarioTopo, myScenarioStates);
+        // create command center
+        ControlCenter controlCenter = topoFactory.createControlCenter();
+        topo.getContainsNE().add(controlCenter);
 
-        return myDTO;
+        // create network node for command center and connect
+        NetworkNode networkNode = topoFactory.createNetworkNode();
+
+//      NetworkNode lastNetworkNode = topo
+
+        // iterate nodes
+        for (Entry<String, Map<String, SmartMeterGeoData>> nodeEntry : smartMeterGeoData.entrySet()) {
+            // create node
+            PowerGridNode powerGridNode = topoFactory.createPowerGridNode();
+            if (!setNameAndId(nodeEntry, powerGridNode))
+                continue;
+            topo.getContainsPGN().add(powerGridNode);
+
+            // create netork node
+            networkNode = topoFactory.createNetworkNode();
+
+            // iterate smart meter
+            for (Entry<String, SmartMeterGeoData> smartMeterEntry : nodeEntry.getValue().entrySet()) {
+                // create smart meter
+                SmartMeter smartMeter = topoFactory.createSmartMeter();
+                if (!setNameAndId(smartMeterEntry, smartMeter))
+                    continue;
+                topo.getContainsNE().add(smartMeter);
+
+                // connect to power
+                smartMeter.getConnectedTo().add(powerGridNode);
+
+                // connect the smart meter
+                createPhysicalConnection(topoFactory, topo, networkNode, smartMeter);
+                createLogicalConnection(topoFactory, topo, controlCenter, smartMeter);
+
+            }
+        }
+
+        // connect command center to power
+        PowerGridNode firstNode = topo.getContainsPGN().get(0);
+        controlCenter.getConnectedTo().add(firstNode);
+
+        // connect command center to the network
+
+//        // Init Scenario State
+//        SmartgridinputPackageImpl.init();
+//        final SmartgridinputFactory inputFactory = SmartgridinputFactory.eINSTANCE;
+//        final ScenarioState input = inputFactory.createScenarioState();
+    }
+
+    public LogicalCommunication createLogicalConnection(final SmartgridtopoFactory topoFactory, final SmartGridTopology topo, ControlCenter controlCenter, SmartMeter smartMeter) {
+        LogicalCommunication logicalCommunication = topoFactory.createLogicalCommunication();
+        smartMeter.getCommunicatesBy().add(logicalCommunication);
+        controlCenter.getCommunicatesBy().add(logicalCommunication);
+        topo.getContainsLC().add(logicalCommunication);
+        return logicalCommunication;
+    }
+
+    public PhysicalConnection createPhysicalConnection(final SmartgridtopoFactory topoFactory, final SmartGridTopology topo, NetworkNode networkNode, SmartMeter smartMeter) {
+        PhysicalConnection physicalConnection = topoFactory.createPhysicalConnection();
+        smartMeter.getLinkedBy().add(physicalConnection);
+        networkNode.getLinkedBy().add(physicalConnection);
+        topo.getContainsPC().add(physicalConnection);
+        return physicalConnection;
+    }
+
+    public boolean setNameAndId(Entry<String, ?> nodeEntry, NamedIdentifier entity) {
+        String nodeKey = nodeEntry.getKey();
+        entity.setName(nodeKey);
+        int nodeId;
+        try {
+            nodeId = Integer.parseInt(nodeKey);
+        } catch (NumberFormatException e) {
+            LOG.error("Could not convert node key to int (" + nodeKey + "). Skipping this node.");
+            return false;
+        }
+        entity.setId(nodeId);
+        return true;
     }
 }
