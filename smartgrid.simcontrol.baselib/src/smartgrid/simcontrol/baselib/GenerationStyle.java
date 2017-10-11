40bf10efeecbde6e009c1abadba5f8b51be9b88e
MishaStrittmatter <strittmatter@kit.edu>
2017-10-11 13:31:14 +0000

    minor
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34644 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.baselib/src/smartgrid/simcontrol/baselib/GenerationStyle.java
+++ smartgrid.simcontrol.baselib/src/smartgrid/simcontrol/baselib/GenerationStyle.java
@@ -1,13 +1,10 @@
-/**
- *
- */
 package smartgrid.simcontrol.baselib;
 
 /**
- * Defines supported Modes to generate synthetic Input models for smartgrid analysis
+ * Defines supported Modes to generate synthetic Input models for smartgrid analysis. Currently
+ * unused.
  *
  * @author Christian
- *
  */
 public enum GenerationStyle {
 
