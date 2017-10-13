833ba393ec3510c4b3dadd8bf7addd1461435fa5
MishaStrittmatter <strittmatter@kit.edu>
2017-10-13 13:30:12 +0000

    removed info popup on interrupt
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34668 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.ui/src/smartgrid/simcontrol/ui/toolbar/InterruptButtonHandler.java
+++ smartgrid.simcontrol.ui/src/smartgrid/simcontrol/ui/toolbar/InterruptButtonHandler.java
@@ -2,25 +2,25 @@ package smartgrid.simcontrol.ui.toolbar;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 import smartgrid.simcontrol.rmi.BlockingKritisDataExchanger;
-import smartgrid.simcontrol.ui.EclipseUiHelper;
 
 public class InterruptButtonHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) {
-        boolean threadsFreed = BlockingKritisDataExchanger.freeAll();
-        String message;
-        if (threadsFreed) {
-            message = "Threads were freed from the synchronization.";
-        } else {
-            message = "There were no threads waiting.";
-        }
-        Shell shell = HandlerUtil.getActiveShell(event);
-        EclipseUiHelper.prompt(message, shell);
+        BlockingKritisDataExchanger.freeAll();
+
+        /* This code shows a popup that is not really necessary anymore */
+//        boolean threadsFreed = BlockingKritisDataExchanger.freeAll();
+//        String message;
+//        if (threadsFreed) {
+//            message = "Threads were freed from the synchronization.";
+//        } else {
+//            message = "There were no threads waiting.";
+//        }
+//        Shell shell = HandlerUtil.getActiveShell(event);
+//        EclipseUiHelper.prompt(message, shell);
         return null;
     }
 }
