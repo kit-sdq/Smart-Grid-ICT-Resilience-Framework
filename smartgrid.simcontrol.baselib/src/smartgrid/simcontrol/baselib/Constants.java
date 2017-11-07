26aca537d1d808b6ee873a518e2c87246d3a71fe
MishaStrittmatter <strittmatter@kit.edu>
2017-11-07 09:26:33 +0000

    minor: removed redundant comments, reordering of fields (in a more meaningful way)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34704 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.baselib/src/smartgrid/simcontrol/baselib/Constants.java
+++ smartgrid.simcontrol.baselib/src/smartgrid/simcontrol/baselib/Constants.java
@@ -1,172 +1,48 @@
-/**
- *
- */
 package smartgrid.simcontrol.baselib;
 
-/**
- * Defines the Constants used as Keys in the {@link org.eclipse.debug.core#ILaunchConfiguration}
- * Dict
- *
- * @author Christian
- *
- */
 public final class Constants {
-
-    /*
-     * Boolean Strings
-     */
-    /**
-     * String Constant for Boolean True
-     */
+    // Boolean Strings
     public static final String TRUE = "true"; //$NON-NLS-1$
-    /**
-     * String Constant for Boolean False
-     */
     public static final String FALSE = "false"; //$NON-NLS-1$
-
-    /**
-     * String Constant for failure
-     */
     public static final String FAIL = "fail"; //$NON-NLS-1$
 
-    /*
-     * Constants for the KEYs of the Working Copy Dict
-     */
-    // TODO Change to Dict implementation friendly keys?
-    /**
-     * Defines the OUTPUT_PATH_KEY
-     */
-    public static final String OUTPUT_PATH_KEY = "outputPath"; //$NON-NLS-1$
-    /**
-     * Defines the TOPOLOGY_PATH_KEY
-     */
-    public static final String TOPOLOGY_PATH_KEY = "topologyPath"; //$NON-NLS-1$
-    /**
-     * Defines the INPUT_PATH_KEY
-     */
-    public static final String INPUT_PATH_KEY = "inputPath"; //$NON-NLS-1$
-
-    /**
-     *
-     * Defines NODE PATH
-     *
-     * The path to File on Disk with a On EntityStates List
-     *
-     */
-    public static final String NODE_PATH = "nodepath"; //$NON-NLS-1$
-
-    /**
-     * Defines the TIMESTEPS_KEY
-     */
-    public static final String TIMESTEPS_KEY = "timeSteps"; //$NON-NLS-1$
-
-    /**
-     * Defines Iteration Count key
-     */
-    public static final String ITERATION_COUNT_KEY = "iterationCount";
-
-    /**
-     * Defines the IGNORE_LOC_CON_KEY
-     */
-    public static final String IGNORE_LOC_CON_KEY = "logicalCon"; //$NON-NLS-1$
-
-    /**
-     * Defines DEFAULT_IGNORE_LOC_CON_KEY
-     */
-    public static final String DEFAULT_IGNORE_LOC_CON = Constants.TRUE;
-
-    /*
-     * Constants for the Default Input/Output File Paths
-     */
-    /**
-     * Defines the DEFAULT_PATH
-     */
+    // default input/output file paths
     public static final String DEFAULT_PATH = ""; //$NON-NLS-1$
-    /**
-     * Defines the DEFAULT_INPUT_PATH
-     */
+    public static final String INPUT_PATH_KEY = "inputPath"; //$NON-NLS-1$
     public static final String DEFAULT_INPUT_PATH = DEFAULT_PATH;
-    /**
-     * Defines the DEFAULT_OUTPUT_PATH
-     */
+    public static final String OUTPUT_PATH_KEY = "outputPath"; //$NON-NLS-1$
     public static final String DEFAULT_OUTPUT_PATH = DEFAULT_PATH;
-    /**
-     * Defines the DEFAULT_TOPO_PATH
-     */
+    public static final String TOPOLOGY_PATH_KEY = "topologyPath"; //$NON-NLS-1$
     public static final String DEFAULT_TOPO_PATH = DEFAULT_PATH;
-    /**
-     * Defines the DEFAULT_TIME_STEPS
-     */
-    public static final String DEFAULT_TIME_STEPS = "1";
-    /**
-     * Default number of iterations per time step
-     */
-    public static final String DEFAULT_ITERATION_COUNT = "2";
 
-    /*
-     * Constants for valid Extensions
-     */
-    /**
-     * Defines the TOPO_EXTENSION
-     */
+    // file extensions
     public static final String TOPO_EXTENSION = "smartgridtopo"; //$NON-NLS-1$
-    /**
-     * Defines the INPUT_EXTENSION
-     */
     public static final String INPUT_EXTENSION = "smartgridinput"; //$NON-NLS-1$
-    /**
-     * Defines the OUTPUT_EXTENSION
-     */
     public static final String OUTPUT_EXTENSION = "smartgridoutput"; //$NON-NLS-1$
 
-    /*
-     * Constants for Local Hacker
-     */
-    /**
-     * Defines the HACKING_STYLE_KEY
-     */
+    // further settings
+    public static final String IGNORE_LOC_CON_KEY = "logicalCon"; //$NON-NLS-1$
+    public static final String DEFAULT_IGNORE_LOC_CON = Constants.TRUE;
+    public static final String TIMESTEPS_KEY = "timeSteps"; //$NON-NLS-1$
+    public static final String DEFAULT_TIME_STEPS = "1";
+    public static final String ITERATION_COUNT_KEY = "iterationCount";
+    public static final String DEFAULT_ITERATION_COUNT = "2";
+
+    public static final String NODE_PATH = "nodepath"; // The path to File on Disk with a On EntityStates List
+
+    // local hacker
     public static final String HACKING_STYLE_KEY = "desiredHackingStyle"; //$NON-NLS-1$
-
-    /**
-     * Defines DEFAULT_HACKING_STYLE
-     */
     public static final String DEFAULT_HACKING_STYLE = "BFS_HACKING"; //$NON-NLS-1$
-
-    /**
-     * Defines the HACKING_SPEED
-     */
     public static final String HACKING_SPEED_KEY = "hackingSpeed"; //$NON-NLS-1$
-
-    /**
-     * Defines the DEFAULT_HACKING_SPEED
-     */
     public static final String DEFAULT_HACKING_SPEED = "1"; //$NON-NLS-1$
-
-    /**
-     * Defines the ROOT_NODE_ID_KEY
-     */
     public static final String ROOT_NODE_ID_KEY = "rootNodeID"; //$NON-NLS-1$
-
-    /**
-     * Defines DEFAULT_ROOT_NODE_ID
-     */
     public static final String DEFAULT_ROOT_NODE_ID = "-1"; //$NON-NLS-1$
 
-    /*
-     * Constants for Viral Hacker
-     */
-    /**
-     * Defines DEFAULT_NODE_MODE
-     */
+    // viral hacker
     public static final String DEFAULT_NODE_MODE = "RandomNode"; //$NON-NLS-1$
-    /**
-     * Defines NODE_MODE
-     */
     public static final String NODE_MODE = "nodeMode"; //$NON-NLS-1$
 
-    /**
-     * Constants for Extension Point ID's
-     */
+    // extension point IDs
     public static final String ATTACKER_SIMULATION_EXTENSION_POINT = "smartgrid.simcontrol.extensionpoints.attackerSimulation";
     public static final String IMPACT_ANALYSIS_EXTENSION_POINT = "smartgrid.simcontrol.extensionpoints.impactAnalysis";
     public static final String TERMINATION_CONDITION_EXTENSION_POINT = "smartgrid.simcontrol.extensionpoints.terminationCondition";
@@ -175,6 +51,7 @@ public final class Constants {
     public static final String INPUT_GERNATOR_EXTENSION_POINT = "smartgrid.simcontrol.extensionpoints.inputGenerator";
     public static final String KRITIS_SIMULATION_EXTENSION_POINT = "smartgrid.simucontrol.extensionpoints.kritisSimulation";
 
+    // extension point keys
     public static final String POWER_LOAD_SIMULATION_CONFIG = "powerLoadConfiguration";
     public static final String ATTACKER_SIMULATION_CONFIG = "attackerSimulationConfiguration";
     public static final String IMPACT_ANALYSIS_SIMULATION_CONFIG = "impactAnalysisConfiguration";
@@ -182,5 +59,4 @@ public final class Constants {
     public static final String TIME_PROGRESSOR_SIMULATION_CONFIG = "timeProgressorConfiguration";
     public static final String INPUT_GENERATOR_SIMULATION_CONFIG = "inputGeneratorConfiguration";
     public static final String KRITIS_SIMULATION_CONFIG = "kritisSimulationConfiguration";
-
 }
