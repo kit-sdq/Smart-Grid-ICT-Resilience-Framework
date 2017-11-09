2218a7525b33329370476217ff0e1737d9701163
MishaStrittmatter <strittmatter@kit.edu>
2017-11-09 14:16:58 +0000

    minor: delete dead class
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34716 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.coupling/src/smartgrid/simcontrol/coupling/IKritisSimulationNew.java
+++
@@ -1,9 +0,0 @@
-package smartgrid.simcontrol.coupling;
-
-import java.util.Map;
-
-public interface IKritisSimulationNew {
-    Map<String, Map<String, PowerSpec>> run(Map<String, Map<String, Double>> power);
-
-    Map<String, Map<String, PowerSpec>> getDefaultDemand();
-}
