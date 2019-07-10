249d8ca97327569a2c6099700a2506e4c8b3f07e
MazenEbada <utxqw@student.kit.edu>
2019-07-10 01:28:49 +0000

    Remove old dependencies/Remove KRITIS/POWER Simulation.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34952 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- smartgrid.simcontrol.test.baselib/src/smartgrid/simcontrol/test/baselib/coupling/IPowerLoadSimulationWrapper.java
+++ smartgrid.simcontrol.test.baselib/src/smartgrid/simcontrol/test/baselib/coupling/IPowerLoadSimulationWrapper.java
@@ -1,36 +1,36 @@
-package smartgrid.simcontrol.test.baselib.coupling;
-
-import java.util.Map;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.ILaunchConfiguration;
-
-import smartgrid.simcontrol.coupling.ISmartMeterState;
-import couplingToICT.PowerSpec;
-import couplingToICT.SmartGridTopoContainer;
-
-public interface IPowerLoadSimulationWrapper extends ISimulationComponent {
-
-    /**
-     * If using ExtensionPoints and so 0-parameter Constructor pass the config from Simcontrol UI to
-     * this Method to build the desired AttackerSimulation ("Factory Method")
-     *
-     * @param config
-     *            behavior for the Attacker
-     * @return true if Init was successful
-     * @throws CoreException
-     *             If ILaunchConfiguration.getAttribute fails
-     */
-    public void init(ILaunchConfiguration config) throws CoreException;
-
-    public void initData(SmartGridTopoContainer topoData);
-
-    /**
-     * @param kritisDemands
-     *            Map von Node ID (String) auf Map von CI ID (String) auf PowerSpecs
-     * @param smartMeterStates
-     *            Map von Node ID auf Map von CI ID auf SmartMeterState
-     * @return Map von Node ID (String) auf Map von CI ID (String) auf Leistung (MW)
-     */
-    Map<String, Map<String, Double>> run(Map<String, Map<String, PowerSpec>> kritisDemands, Map<String, Map<String, ISmartMeterState>> smartMeterStates);
-}
+//package smartgrid.simcontrol.test.baselib.coupling;
+//
+//import java.util.Map;
+//
+//import org.eclipse.core.runtime.CoreException;
+//import org.eclipse.debug.core.ILaunchConfiguration;
+//
+//import smartgrid.simcontrol.coupling.ISmartMeterState;
+//import couplingToICT.PowerSpec;
+//import couplingToICT.SmartGridTopoContainer;
+//
+//public interface IPowerLoadSimulationWrapper extends ISimulationComponent {
+//
+//    /**
+//     * If using ExtensionPoints and so 0-parameter Constructor pass the config from Simcontrol UI to
+//     * this Method to build the desired AttackerSimulation ("Factory Method")
+//     *
+//     * @param config
+//     *            behavior for the Attacker
+//     * @return true if Init was successful
+//     * @throws CoreException
+//     *             If ILaunchConfiguration.getAttribute fails
+//     */
+//    public void init(ILaunchConfiguration config) throws CoreException;
+//
+//    public void initData(SmartGridTopoContainer topoData);
+//
+//    /**
+//     * @param kritisDemands
+//     *            Map von Node ID (String) auf Map von CI ID (String) auf PowerSpecs
+//     * @param smartMeterStates
+//     *            Map von Node ID auf Map von CI ID auf SmartMeterState
+//     * @return Map von Node ID (String) auf Map von CI ID (String) auf Leistung (MW)
+//     */
+//    Map<String, Map<String, Double>> run(Map<String, Map<String, PowerSpec>> kritisDemands, Map<String, Map<String, ISmartMeterState>> smartMeterStates);
+//}
