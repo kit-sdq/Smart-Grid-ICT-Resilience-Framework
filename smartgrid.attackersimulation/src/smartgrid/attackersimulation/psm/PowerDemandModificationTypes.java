db207f21797e461bd2a9b0d96be092eabedf7a8c
anonymous <anonymous>
2019-09-25 12:25:57 +0000

    RmiServer update 25.09
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SmartGrid@34982 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ smartgrid.attackersimulation/src/smartgrid/attackersimulation/psm/PowerDemandModificationTypes.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package smartgrid.simcontrol.test.util;
+
+/**
+ * @author Mazen
+ *
+ */
+public enum PowerDemandModificationTypes {
+ 
+    MAX_DEMAND,
+ 
+    ZERO_DEMAND
+    
+    
+
+}
---
+++ smartgrid.attackersimulation/src/smartgrid/attackersimulation/psm/ZeroPDM.java
@@ -0,0 +1,53 @@
+package smartgrid.simcontrol.test.util;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import couplingToICT.PowerDemand;
+import couplingToICT.PowerSpec;
+
+public class ZeroPDM implements PowerDemandModifier {
+
+    @Override
+    public PowerDemand modifyPowerDemand(Map<String, Map<String, PowerSpec>> oldPowerDemandsHashMap) {
+
+        
+        LinkedHashMap<String, HashMap<String, PowerSpec>> newPowerDemandsHashMap = new LinkedHashMap<String, HashMap<String,PowerSpec>>();
+        
+        for (String powerDistrictId : oldPowerDemandsHashMap.keySet()) {
+            
+            Map<String, PowerSpec> oldSmartMeterPowerSpecs = oldPowerDemandsHashMap.get(powerDistrictId);
+            HashMap<String, PowerSpec> newSmartMeterPowerSpecsHashMap = new HashMap<String, PowerSpec>();
+            
+            for (String smartMeterID : oldSmartMeterPowerSpecs.keySet()) {
+                PowerSpec powerSpec = oldSmartMeterPowerSpecs.get(smartMeterID);
+                
+                String _ciType = powerSpec.getCiType();
+                String _ciName = powerSpec.getCiName();
+                String _ciSmartID = powerSpec.getCiSmartID();
+                String _ciID = powerSpec.getCiID();
+                int _powerDistrictId = powerSpec.getPowerDistrictId();
+                int _aggregation = powerSpec.getAggregation();
+                double _minReqDemand = powerSpec.getMinReqDemand();
+                double _criticality = powerSpec.getCriticality();
+                
+                // change power
+                double _optDemand = 0;
+                
+                PowerSpec newPowerSpec =  new PowerSpec(_ciType, _ciName, _ciID,
+                        _ciSmartID, _powerDistrictId, _aggregation, _optDemand, _minReqDemand, _criticality);
+                
+                newSmartMeterPowerSpecsHashMap.put(smartMeterID, newPowerSpec);
+            }
+            
+            newPowerDemandsHashMap.put(powerDistrictId, newSmartMeterPowerSpecsHashMap);
+        }
+        
+        PowerDemand newPowerDemand = new PowerDemand(newPowerDemandsHashMap);
+        
+        return newPowerDemand;
+    }
+
+
+}
