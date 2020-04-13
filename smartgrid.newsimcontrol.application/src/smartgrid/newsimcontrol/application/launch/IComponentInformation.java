77e48408fbd30fdfe9c478c04c104aa92973ef82
Mazen Ebada <mazennabilebada.1@gmail.com>
2020-04-13 21:53:17 +0200

    Deleted old not needed code

--- smartgrid.newsimcontrol.application/src/smartgrid/newsimcontrol/application/launch/IComponentInformation.java
+++
@@ -1,6 +0,0 @@
-package smartgrid.newsimcontrol.application.launch;
-
-public interface IComponentInformation {
-	ILoadMe getComponent();
-	String getId();
-}
--- smartgrid.newsimcontrol.application/src/smartgrid/newsimcontrol/application/launch/IComponentLoader.java
+++
@@ -1,7 +0,0 @@
-package smartgrid.newsimcontrol.application.launch;
-
-import java.util.Collection;
-
-public interface IComponentLoader {
-	Collection<IComponentInformation> getComponents();
-}
